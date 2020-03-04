61709f3e21641120612cbef929aedb8a16315b28
saigonomai <otonashi13@hotmail.com>
2020-03-04 11:20:46 -0500

    implemented color scaling for markers

--- client/src/components/Pin/index.js.js
+++ client/src/components/Pin/index.js.js
@@ -21,7 +21,7 @@ export default class Pins extends PureComponent {
           viewBox="0 0 24 24"
           style={{
             cursor: 'pointer',
-            fill: '#d00',
+            fill: city.fill,
             stroke: 'none',
             transform: `translate(${-SIZE / 2}px,${-SIZE}px)`
           }}
